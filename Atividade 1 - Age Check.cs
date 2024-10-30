7890e6e952fd7cefd70ba49e6f091d43f195b79e
Júlio César <juliosbto@gmail.com>
2024-10-29 21:12:55 -0300

    Atividade Revisada.
    Atividade introdutória para revisar o uso da linguagem C#

--- Atividade 1 - Age Check.cs
+++ Atividade 1 - Age Check.cs
@@ -1,26 +1,27 @@
 using System;
-					
+
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Digite o seu nome: ");
-		string nome = Console.ReadLine();
-		
-		Console.WriteLine("Digite a sua idade: ");
-		int idade = int.Parse(Console.ReadLine());
-		
-		Console.WriteLine("Seu nome é " + nome + " e você tem " + idade + " anos.");
-		
-		if (idade >= 18)
-		{
-		    Console.WriteLine("Você é maior de idade.");
-		}
-		else
-	    {
-	        Console.WriteLine("Você é Menor de idade.");
-	    }
-	    
-	    Console.ReadKey();
+        Console.Write("Digite o seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite a sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Seu nome é {nome} e você tem {idade} anos.");
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("Você é maior de idade.");
+        }
+        else
+        {
+            Console.WriteLine("Você é menor de idade.");
+        }
+
+        Console.WriteLine("Pressione qualquer tecla para sair.");
+        Console.ReadKey();
     }
 }
