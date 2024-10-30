8981970ba470ff3abf9c6e26057d411aa1dde3af
Júlio César <juliosbto@gmail.com>
2024-10-29 21:14:41 -0300

    Aprimoramento do código.
    Código revisado e aprimorado.

--- Atividade 2 - Calculadora.cs
+++ Atividade 2 - Calculadora.cs
@@ -1,46 +1,52 @@
 using System;
-					
+
 public class Program
 {
     public static void Main(string[] args)
     {
         Console.WriteLine("Escolha a operação que deseja realizar:");
-	    Console.WriteLine("+ = Soma.");
-	    Console.WriteLine("- = Subtração.");
-	    Console.WriteLine("* = Multiplicação.");
-	    Console.WriteLine("/ = Divisão.");
-	    char operacao = char.Parse(Console.ReadLine());
-	    
-	    Console.WriteLine("Insira o 1º Número: ");
-		int num1 = int.Parse(Console.ReadLine());
-		
-		Console.WriteLine("Insira o 2º Número: ");
-		int num2 = int.Parse(Console.ReadLine());
-		
-	    if (operacao == '+')
-	    {
-	        Console.WriteLine(num1 + " " + operacao + " " + num2 + " = " + (num1 + num2));
-	    }
-	    else if (operacao == '-')
-	    {
-	        Console.WriteLine(num1 + " " + operacao + " " + num2 + " = " + (num1 - num2));
-	    }
-	    else if (operacao == '*')
-	    {
-	        Console.WriteLine(num1 + " " + operacao + " " + num2 + " = " + (num1 * num2));
-	    }
-	    else if (operacao == '/')
-	    {
-	        if (num2 == 0)
-	        {
-	            Console.WriteLine("Não é possível dividir por zero.");
-	        }
-	        else
-	        {
-	            Console.WriteLine(num1 + " " + operacao + " " + num2 + " = " + (num1 / num2));
-	        }
-	    }
-	    
-	    Console.ReadKey();
+        Console.WriteLine("+ = Soma");
+        Console.WriteLine("- = Subtração");
+        Console.WriteLine("* = Multiplicação");
+        Console.WriteLine("/ = Divisão");
+
+        char operacao = char.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira o 1º Número:");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Insira o 2º Número:");
+        double num2 = double.Parse(Console.ReadLine());
+
+        if (operacao == '+')
+        {
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        }
+        else if (operacao == '-')
+        {
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+        }
+        else if (operacao == '*')
+        {
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+        }
+        else if (operacao == '/')
+        {
+            if (num2 == 0)
+            {
+                Console.WriteLine("Não é possível dividir por zero.");
+            }
+            else
+            {
+                Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Operação inválida.");
+        }
+
+        Console.WriteLine("Pressione qualquer tecla para sair.");
+        Console.ReadKey();
     }
 }
